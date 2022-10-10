cadc9b54c4330711b7002c1c1632689b98f0f86b
Andy Doan <andy@foundries.io>
2022-10-10 15:23:55 -0500

    Introduce http handler for "simpleenroll"
    Signed-off-by: Andy Doan <andy@foundries.io>

--- http_handlers.go
+++ http_handlers.go
@@ -1,6 +1,9 @@
 package est
 
 import (
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -15,4 +18,33 @@ func RegisterEchoHandlers(svc Service, e *echo.Echo) {
 		}
 		return c.Blob(200, "application/pkcs7-mime", certs)
 	})
+	e.POST("/.well-known/est/simpleenroll", func(c echo.Context) error {
+		bytes, err := validateRequest(svc, c)
+		if err != nil {
+			return err
+		}
+		bytes, err = svc.Enroll(c.Request().Context(), bytes)
+		if err != nil {
+			if errors.Is(err, EstError) {
+				return c.String(http.StatusBadRequest, err.Error())
+			}
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+		return c.Blob(http.StatusCreated, "application/pkcs7-mime", bytes)
+	})
+}
+
+// validateRequest checks that the client has provided a client cert (via mTLS)
+// as per: https://www.rfc-editor.org/rfc/rfc7030.html#section-3.3.2
+// and has set the correct request content-type as per:
+// https://www.rfc-editor.org/rfc/rfc7030.html#section-4.2.1
+func validateRequest(svc Service, c echo.Context) ([]byte, error) {
+	if len(c.Request().TLS.PeerCertificates) != 1 {
+		return nil, c.String(http.StatusUnauthorized, "Client must provide certificate")
+	}
+	ct := c.Request().Header.Get("content-type")
+	if ct != "application/pkcs10" {
+		return nil, c.String(http.StatusBadRequest, fmt.Sprintf("Invalid content-type: %s. Must be application/pkcs10", ct))
+	}
+	return io.ReadAll(c.Request().Body)
 }
--- http_test.go
+++ http_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"crypto/tls"
+	"crypto/x509"
 	"encoding/base64"
 	"io"
 	"net"
@@ -12,6 +13,7 @@ import (
 	"testing"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/random"
 	"github.com/stretchr/testify/require"
 	"go.mozilla.org/pkcs7"
 )
@@ -55,8 +57,11 @@ func WithEstServer(t *testing.T, testFunc func(tc testClient)) {
 	ctx := CtxWithLog(context.TODO(), InitLogger(""))
 	srv := httptest.NewUnstartedServer(e)
 
+	pool := x509.NewCertPool()
+	pool.AddCert(svc.rootCa)
 	srv.TLS = &tls.Config{
 		ClientAuth: tls.VerifyClientCertIfGiven,
+		ClientCAs:  pool,
 	}
 	srv.StartTLS()
 	t.Cleanup(srv.Close)
@@ -81,4 +86,64 @@ func TestCACertificatesRequest(t *testing.T) {
 		require.Nil(t, err)
 		require.Equal(t, tc.svc.ca, p7.Certificates[0])
 	})
+}
+
+func TestSimpleEnrollRequiresCert(t *testing.T) {
+	WithEstServer(t, func(tc testClient) {
+		rc, data := tc.POST(t, "/.well-known/est/simpleenroll", []byte{}, nil)
+		require.Equal(t, 401, rc, string(data))
+	})
+}
+
+func TestSimpleEnrollRequiresValidCert(t *testing.T) {
+	WithEstServer(t, func(tc testClient) {
+		svc := createService(t)
+
+		kp := svc.createTlsKP(t, tc.ctx, "enrollRequiresValid")
+
+		url := tc.srv.URL + ".well-known/est/simpleenroll"
+		client := tc.srv.Client()
+		transport := client.Transport.(*http.Transport)
+		transport.TLSClientConfig.Certificates = []tls.Certificate{*kp}
+
+		_, err := client.Post(url, "application/pkcs10", bytes.NewBuffer([]byte{}))
+		require.NotNil(t, err)
+	})
+}
+
+func TestSimpleEnroll(t *testing.T) {
+	WithEstServer(t, func(tc testClient) {
+		cn := random.String(10)
+		kp := tc.svc.createTlsKP(t, tc.ctx, cn)
+		rc, data := tc.POST(t, "/.well-known/est/simpleenroll", []byte{}, kp)
+		require.Equal(t, 400, rc, string(data))
+		require.Equal(t, "The CSR could not be decoded: asn1: syntax error: sequence truncated", string(data))
+
+		_, csr := createB64CsrDer(t, cn)
+		rc, data = tc.POST(t, "/.well-known/est/simpleenroll", csr, kp)
+		require.Equal(t, 201, rc, string(data))
+
+		buf, err := base64.StdEncoding.DecodeString(string(data))
+		require.Nil(t, err)
+		p7, err := pkcs7.Parse(buf)
+		require.Nil(t, err)
+		cert := p7.Certificates[0]
+		require.Equal(t, cn, cert.Subject.CommonName)
+	})
+}
+
+func (s Service) createTlsKP(t *testing.T, ctx context.Context, cn string) *tls.Certificate {
+	key, csrBytes := createB64CsrDer(t, cn)
+	bytes, err := s.Enroll(ctx, csrBytes)
+	require.Nil(t, err)
+
+	bytes, err = base64.StdEncoding.DecodeString(string(bytes))
+	require.Nil(t, err)
+	p7, err := pkcs7.Parse(bytes)
+	require.Nil(t, err)
+	cert := p7.Certificates[0]
+	return &tls.Certificate{
+		Certificate: [][]byte{cert.Raw},
+		PrivateKey:  key,
+	}
 }
