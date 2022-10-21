2b58d6f2e728a6341840244961a273199790b7e3
Andy Doan <andy@foundries.io>
2022-10-20 20:43:04 -0500

    Log the public key when creating a new cert
    Could be helpful for auditing and debugging
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- service.go
+++ service.go
@@ -9,6 +9,7 @@ import (
 	"crypto/x509/pkix"
 	"encoding/asn1"
 	"encoding/base64"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"math/big"
@@ -265,5 +266,19 @@ func (s Service) signCsr(ctx context.Context, csr *x509.CertificateRequest) ([]b
 		log.Error().Err(err).Msg("Unable to PKCS7 encode certificate")
 		return nil, err
 	}
+	pub, _ := pubkey(cert)
+	log.Info().Str("value", pub).Msg("New certificate for key")
 	return []byte(base64.StdEncoding.EncodeToString(bytes)), nil
+}
+
+func pubkey(cert *x509.Certificate) (string, error) {
+	derBytes, err := x509.MarshalPKIXPublicKey(cert.PublicKey)
+	if err != nil {
+		return "", err
+	}
+	block := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: derBytes,
+	}
+	return string(pem.EncodeToMemory(block)), nil
 }
