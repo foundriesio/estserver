162fb4126d7b9d4d86e3947b2d3c0716fa166ad0
Andy Doan <andy@foundries.io>
2023-12-13 15:51:53 -0600

    handlers: Fix bad error handling
    We had the right intentions with `validateRequest`. However, we weren't
    handling its return value correctly when it actually failed.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- http_handlers.go
+++ http_handlers.go
@@ -28,7 +28,7 @@ func RegisterEchoHandlers(svcHandler ServiceHandler, e *echo.Echo) {
 			return c.String(http.StatusInternalServerError, err.Error())
 		}
 		bytes, err := validateRequest(svc, c)
-		if err != nil {
+		if bytes == nil || err != nil { // validateRequest failed and sent the response
 			return err
 		}
 		bytes, err = svc.Enroll(c.Request().Context(), bytes)
@@ -46,7 +46,7 @@ func RegisterEchoHandlers(svcHandler ServiceHandler, e *echo.Echo) {
 			return c.String(http.StatusInternalServerError, err.Error())
 		}
 		bytes, err := validateRequest(svc, c)
-		if err != nil {
+		if bytes == nil || err != nil { // validateRequest failed and sent the response
 			return err
 		}
 		peerCerts := c.Request().TLS.PeerCertificates
